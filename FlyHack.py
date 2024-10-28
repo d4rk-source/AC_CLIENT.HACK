e2adb57e83c53d79b2a2a724e43389cabc7cf7fe
d4rk-source <adamas.vensl@gmail.com>
2024-10-28 13:26:40 +0200

    Update FlyHack.py
    works, poorly, but works

--- FlyHack.py
+++ FlyHack.py
@@ -1,11 +1,12 @@
 from pymem import *
 from pymem.process import * #pymem is for reding memory
 import keyboard
+import time
 
-shortcut = "F1"
+shortcut = "space"
 mem = Pymem("ac_client.exe") #attaches to the game
 module = module_from_name(mem.process_handle, "ac_client.exe").lpBaseOfDll # gets the address of "ac_client.exe"
-offsets = [0x8, 0x9DC, 0x30, 0x668]
+offsets = [0x30]
 
 def getPointerAddr(base, offsets):
     addr = mem.read_int(base)
@@ -15,8 +16,33 @@ def getPointerAddr(base, offsets):
         addr = addr + offsets[-1]
     return addr
 
-print(getPointerAddr(module + 0x00183828, offsets))
 
-# while True:
-#     if keyboard.is_pressed(shortcut):
-#         mem.write_int(getPointerAddr(module + 0x00183828, offsets), 1095485876)
+#cant figure this part out, allways gives error that it cant read at that address
+# print(getPointerAddr(module + 0x00183828, offsets))
+
+while True:
+    if keyboard.is_pressed(shortcut):
+        # mem.write_int(getPointerAddr(module + 0x0017E0A8, offsets), 1050000000)
+        time.sleep(0.1)
+        addme = mem.read_int(getPointerAddr(module + 0x0017E0A8, offsets))
+        # print(addme)
+        if str(addme)[0] == "-":
+            while mem.read_int(getPointerAddr(module + 0x0017E0A8, offsets)) < 0:
+                while keyboard.is_pressed(shortcut):
+                    addme -= 25000     
+                    mem.write_int(getPointerAddr(module + 0x0017E0A8, offsets), addme)
+                    time.sleep(0.001)
+                    print(mem.read_int(getPointerAddr(module + 0x0017E0A8, offsets)))
+                    if -1073995490 > mem.read_int(getPointerAddr(module + 0x0017E0A8, offsets)):
+                        addme = 1045353216
+                        mem.write_int(getPointerAddr(module + 0x0017E0A8, offsets), 0)
+                        mem.write_int(getPointerAddr(module + 0x0017E0A8, offsets), addme)
+                        while keyboard.is_pressed(shortcut):
+                            addme += 25000
+                            mem.write_int(getPointerAddr(module + 0x0017E0A8, offsets), addme)
+                            time.sleep(0.001)
+        else:
+            while keyboard.is_pressed(shortcut):
+                addme += 25000
+                mem.write_int(getPointerAddr(module + 0x0017E0A8, offsets), addme)
+                time.sleep(0.001)
