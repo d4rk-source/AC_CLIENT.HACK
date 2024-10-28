5e1403fc0988ed70582359b6dffee72dcef6a4ba
d4rk-source <adamas.vensl@gmail.com>
2024-10-29 01:53:01 +0200

    created bhop script
    doesn't work yet

---
+++ BHOPscript.py
@@ -0,0 +1,13 @@
+import keyboard
+import pyautogui
+import time
+
+pyautogui.press('space')
+while True:
+    if keyboard.is_pressed("space"):
+        # Press space continuously while the key is held down
+        while keyboard.is_pressed("space"):
+            pyautogui.press('space')
+            print("space should be pressed")
+            time.sleep(0.1)  # Adjust sleep time as needed                    
+    time.sleep(0.01)  # Slight delay to reduce CPU usage when waiting
